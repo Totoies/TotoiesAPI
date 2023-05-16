89f36037fa71da12e838f362091a608067e815f1
pritam dutta <rohitdutta907@gmail.com>
2023-05-16 09:00:59 +0530

    added function to create our application

---
+++ totoies.go
@@ -0,0 +1,18 @@
+/*
+* Main file to create applications
+ */
+
+package totois
+
+//  Our Application
+type Totoies struct {
+}
+
+var totoies *Totoies
+
+/*
+* Creating our new application to start the server
+ */
+func New() *Totoies {
+	return totoies
+}
